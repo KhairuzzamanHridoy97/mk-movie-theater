31023a33a32e88771f524b7bf9f443316df722ef
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-04-10 14:41:05 +0600

    added home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Routes,} from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h2>React Movie</h2>
-      
+      <Routes>
+        <Switch>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route  path='/home'>
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Routes>      
     </div>
   );
 }

181f78f78c5cf9364e46799448f05fdb7e8b945e
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-04-11 03:26:36 +0600

    added ourMovie in ourMovies

--- src/Pages/Home/OurMovies/OurMovies/OurMovies.js
+++ src/Pages/Home/OurMovies/OurMovies/OurMovies.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import { Container } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Row } from 'react-bootstrap';
+import OurMovie from '../OurMovie/OurMovie';
 
 const OurMovies = () => {
+    const [ourMovies,setOurMovies]= useState([]);
+    useEffect(()=>{
+        fetch('./movies.JSON')
+        .then(res=>res.json())
+        .then(data=>setOurMovies(data))
+    },[])
     return (
-        <div>
-            <h3 className="text-primary my-5">Our Movies</h3>
-        </div>
+        <>
+        <Container>
+
+            <h2 className="text-primary my-5">Our Movies</h2>
+            <Row xs={1} md={2} className="g-4">
+            {
+                ourMovies.slice(0,6).map(
+                    ourMovie=> <OurMovie
+                    ourMovie={ourMovie}
+                    key={ourMovie.id}
+                    >
+
+                    </OurMovie>
+                )
+            }
+
+            </Row>
+        </Container>
+        </>
     );
 };
 

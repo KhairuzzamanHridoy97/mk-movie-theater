d1db926e27b9e14468bde55a8be22d0c13cb1810
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-04-11 13:36:45 +0600

    added movie in movies

--- src/Pages/Movies/Movies/Movies.js
+++ src/Pages/Movies/Movies/Movies.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import { Container } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import Footer from '../../Shared/Footer/Footer';
+import Navigation from '../../Shared/Navigation/Navigation';
+import Movie from '../Movie/Movie';
 
 const Movies = () => {
+    const [movies,setMovies]=useState([]);
+    useEffect(()=>{
+        fetch('./movies.JSON')
+        .then(res=>res.json())
+        .then(data=>setMovies(data))
+    },[])
     return (
         <div>
+            <Navigation></Navigation>
             <h2>Book Your Favourite Movie</h2>
+            <Container>
+
+            {
+                movies.map( movie=>
+                    <Movie 
+                    movie={movie}
+                    key={movie.id}
+                      >
+
+                    </Movie>
+                )
+            }
+            </Container>
+            <Footer></Footer>
         </div>
     );
 };
